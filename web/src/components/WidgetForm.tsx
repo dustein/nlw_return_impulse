ec66b908cc92c4f3affbd0cad2cb964583420b93
DuStein <steindu@gmail.com>
2022-05-08 13:00:40 -0300

    criado IF pra quando clicar na opcao

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -39,30 +39,33 @@ export function WidgetForm () {
      const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null);
 
      return (
-          <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+          <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc   (100vw-2rem)] md:w-auto">
                <header>
                     <span className="text-xl leading-6">Deixe seu Feedback</span>
 
                     <CloseButton />
                </header>
 
-          <div className='flex py-8 gap-2 w-full'>
-               { Object.entries(feedbackTypes).map(([key, value]) => {
-                    return (
-                         <button
-                              key={key as feedbackType}
-                              className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-cornova-500 focus:border-cornova-500 focus:outline-none'
-                              onClick={() => setFeedbackType(key)}
-                              type="button"
-                         >
-                              <img src={value.image.source} alt={value.image.alt} />
-                              <span>{value.title}</span>
-                         </button>
-                    );
-               })}
+{!feedbackType ? (
+               <div className='flex py-8 gap-2 w-full'>
+                    { Object.entries(feedbackTypes).map(([key, value]) => {
+                         return (
+                              <button
+                                   key={key as feedbackType}
+                                   className='bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-cornova-500 focus:border-cornova-500 focus:outline-none'
+                                   onClick={() => setFeedbackType(key)}
+                                   type="button"
+                              >
+                                   <img src={value.image.source} alt={value.image.alt} />
+                                   <span>{value.title}</span>
+                              </button>
+                         );
+                    })}
           </div>
+) : (
+     <p>Hello World</p>
+)}
 
-               <p>Hello World</p>
 
                <footer className="text-xs text-neutral-400">
                     Feito no curso <a className="underline underline-offset-4" href="https://dustein.com">Rocketseat</a>
