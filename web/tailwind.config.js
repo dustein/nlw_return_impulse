068adbcf41e93a956faf9bceb2a5684d7a67387d
dustein <steindu@gmail.com>
2022-05-09 14:00:13 -0300

    note work

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
     extend: {
       colors: {
         cornova: {
+          300: '#996DFF',
           500: '#825786'
         }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ],
 }
