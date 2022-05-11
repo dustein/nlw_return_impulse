e0298611b4c751fca4c26a885833f1a78240c104
dustein <steindu@gmail.com>
2022-05-11 16:47:01 -0300

    1h20m

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -1,3 +1,4 @@
+import { FeedbacksRepository } from "../repositories/feedbacks-repository";
 import { PrismaFeedbacksRepository } from "../repositories/prisma/prisma-feedbacks-repository";
 
 interface SubmitFeedbackUseCaseRequest {
@@ -7,16 +8,25 @@ interface SubmitFeedbackUseCaseRequest {
 }
 
 export class SubmitFeedbackUseCase {
+   
+   private feedbacksRepository: FeedbacksRepository
+
+   constructor(
+      feedbacksRepository: FeedbacksRepository,
+   ) {
+      this.feedbacksRepository = feedbacksRepository
+   }
+
    async execute(request: SubmitFeedbackUseCaseRequest) {
       const { type, comment, screenshot } = request;
 
       //se nao quisessemos seguir o principio da inversao de dependencias do SOLID para salvar no BD
-      const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+      // const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
 
-      await prismaFeedbacksRepository.create({
-         type,
-         comment,
-         screenshot,
-      })
+      // await prismaFeedbacksRepository.create({
+      //    type,
+      //    comment,
+      //    screenshot,
+      // })
    }
 }
