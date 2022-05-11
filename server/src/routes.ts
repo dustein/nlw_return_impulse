8f79e80160b4b561452c2fdc9461d6743e12f8ce
dustein <steindu@gmail.com>
2022-05-11 16:27:57 -0300

    continuando

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 export const routes = express.Router();
+
 const transport = nodemailer.createTransport({
    host: "smtp.mailtrap.io",
    port: 2525,
@@ -19,13 +20,7 @@ routes.get("/", (req, res) => {
 routes.post("/feedbacks", async (req, res) => {
    const { type, comment, screenshot } = req.body;
 
-   const feedback = await prisma.feedback.create({
-      data: {
-         type,
-         comment,
-         screenshot,
-      }
-   })
+   const feedback = 
 
    await transport.sendMail({
       from: "DuStein Projeto NLW RETURN <ola@projeto.com>",
