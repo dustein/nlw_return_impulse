709b51d5df0b78b5147bd76b090bc02387df9199
dustein <steindu@gmail.com>
2022-05-11 15:43:32 -0300

    separadas as rotas no arquivo route para iniciar implementacao do SOLID

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,50 +1,12 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
-import { prisma } from './prisma';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const transport = nodemailer.createTransport({
-   host: "smtp.mailtrap.io",
-   port: 2525,
-   auth: {
-     user: "3d77a16cf70168",
-     pass: "8c0d12a242c4ba"
-   }
- });
-
-app.get("/", (req, res) => {
-   res.send("Hello World")
-})
-
-app.post("/feedbacks", async (req, res) => {
-   const { type, comment, screenshot } = req.body;
-
-   const feedback = await prisma.feedback.create({
-      data: {
-         type,
-         comment,
-         screenshot,
-      }
-   })
-
-   await transport.sendMail({
-      from: "DuStein Projeto NLW RETURN <ola@projeto.com>",
-      to: "Destinatario <email@email.com>",
-      subject: "Novo feedback",
-      html: [
-         `<div style="font-family: sans-serif; font-size: 18px; color: #111;">`,
-         `<p>Tipo do feedback: ${type}</p>`,
-         `<p>Comentario: ${comment}>`,
-         `</div>`
-      ].join('\n')
-   });
-
-   return  res.status(201).json({data: feedback});
-});
 
 
+app.use(routes);
 
 app.listen(3333, () => console.log(`Servidor ATIVO !`))
