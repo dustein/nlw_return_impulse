d5d83537752dfc4a583467533549e35d30bc4009
dustein <steindu@gmail.com>
2022-05-12 13:35:44 -0300

    instalado o jest

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,19 @@
-test('sum 2 + 2', () => {
-   expect(2 + 2).toBe(4)
-});
+// test('sum 2 + 2', () => {
+//    expect(2 + 2).toBe(4)
+// });
+
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
+
+describe('Submit Feedback', () => {
+   it('deveria ser capaz de enviar um feedback na aplicacao', async () => {
+      const submitFeedback = new SubmitFeedbackUseCase(
+         { create: async () => {} },
+         { sendMail: async () => {} }
+      )
+      await expect(submitFeedback.execute({
+         type: 'BUG',
+         comment: 'exemplo de comment',
+         screenshot: 'teste.imagem'
+      })).resolves.not.toThrow();
+   })
+})
