793f5a674993dfe051c6b008896e60b4ee487158
dustein <steindu@gmail.com>
2022-05-17 14:18:06 -0300

    inicia integracao

--- web/src/components/widgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/widgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowArcLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -14,17 +16,29 @@ export function FeedbackContentStep ({feedbackType, onFeedbackRestartRequested, 
   
   const [screenshot, setScreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
   
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(Event: FormEvent) {
+  async function handleSubmitFeedback(Event: FormEvent) {
 
 
     Event.preventDefault();
-    console.log({
-      screenshot,
-      comment
-    })
+
+    // console.log({
+    //   screenshot,
+    //   comment
+    // })
+
+    setIsSendingFeedback(true);
+
+    await api.post('feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot
+    });
+
+    setIsSendingFeedback(false);
 
     onFeedbackSent();
   }
@@ -61,9 +75,13 @@ export function FeedbackContentStep ({feedbackType, onFeedbackRestartRequested, 
           </button> */}
           <ScreenshotButton screenshot={screenshot} onScreenshotTook={setScreenshot} />
 
-          <button type="submit" className="p-2 bg-cornova-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-cornova-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-cornova-500 transitions-colors disabled:opacity-50 disabled:hover:bg-cornova-500" disabled={comment.length === 0}>
+          <button 
+            type="submit" 
+            disabled={comment.length === 0 || isSendingFeedback}
+            className="p-2 bg-cornova-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-cornova-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-cornova-500 transitions-colors disabled:opacity-50 disabled:hover:bg-cornova-500" 
+          >
         
-            Enviar Feedback
+            {isSendingFeedback? <Loading /> : 'Enviar Feedback'}
           </button>
         
         </footer>
