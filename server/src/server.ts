a8d649a9aa99cb9baecc10ffdb7f11ac096e60ec
dustein <steindu@gmail.com>
2022-05-18 13:42:14 -0300

    dando server error

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,4 +15,4 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => console.log(`Servidor ATIVO !`));
+app.listen(process.env.PORT, () => console.log(`Servidor ATIVO !`));
