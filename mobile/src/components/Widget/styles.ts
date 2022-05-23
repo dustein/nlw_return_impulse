5243d579e7f27dbab3db74f705cfa9ef379a2885
dustein <steindu@gmail.com>
2022-05-23 14:18:25 -0300

    note work

---
+++ mobile/src/components/Widget/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import { theme } from "../../theme";
+
+export const styles = StyleSheet.create({
+   button: {
+      width: 48,
+      height: 48,
+      borderRadius: 24,
+      backgroundColor: theme.colors.brand,
+      justifyContent: 'center',
+      position: 'absolute',
+      rigth: 16,
+      bottom: getBottomSpace() + 16
+   }
+})
